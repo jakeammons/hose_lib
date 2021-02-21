13d7747efa125f6f7a234252abc4e80b7955618a
Jake Ammons <jakeammons@gmail.com>
2021-02-21 15:41:02 -0500

    broke out encoder functionality into magnetic encoder and quadrature encoder classes

--- include/capstan.h
+++ include/capstan.h
@@ -6,13 +6,14 @@
 #include <AS5600.h>
 #include <Wire.h>
 #include <EEPROM.h>
+#include "encoder.h"
 
 // provides encoder, motor driver, and pid control functions
 // setpoint and input are relative angle in signed degrees (-inf, inf)
 // output is duty cycle in signed pwm values (-255, 255)
 class Capstan {
     public:
-        Capstan(uint8_t dir, uint8_t pwm, uint8_t flt, uint8_t cs, uint8_t mux, uint8_t enc, double kp, double ki, double kd, double circumference, double max_velocity);
+        Capstan(uint8_t dir, uint8_t pwm, uint8_t flt, uint8_t cs, double kp, double ki, double kd, double circumference, double max_velocity, Encoder *enc);
         void init(uint8_t id, bool reset_zero);
         double get_angle();
         void set_angle(double angle);
@@ -25,8 +26,6 @@ class Capstan {
         uint8_t _pwm; // capstan motor driver pwm pin
         uint8_t _flt; // capstan motor driver fault pin
         uint8_t _cs; // capstan motor driver current sense pin
-        uint8_t _mux; // capstan encoder i2c mux address
-        uint8_t _enc; // capstan encoder i2c mux channel
         double _setpoint, _input, _output; // pid input/output
         double _kp = 0, _ki = 0, _kd = 0; // pid gains
         double _previous_angle; // absolute angle of last reading [deg]
@@ -34,6 +33,7 @@ class Capstan {
         int8_t _revolutions; // number of full revolutions relative to zero
         double _circumference; // capstan circumference [mm]
         double _max_velocity; // maximum velocity allowed for tendon movements [mm/ms]
+        Encoder *encoder;
         AMS_5600 ams5600;
         PID pid;
         double calc_angle();
